cbba2ad5f8fae992cd071b557e9a881546587e1b
Zach Wily <zach@zwily.com>
2017-02-22 05:27:19 -0700

    add gergich to build
    Change-Id: I41bad34f2b2be2301e5b6f5c86e3f001f219e2a6

--- build.sh
+++ build.sh
@@ -2,6 +2,11 @@
 
 DC="docker-compose -f docker-compose.yml -f docker-compose.jenkins.yml"
 
+# Clear this out, so jenkins doesn't re-publish something from a previous
+# build.
+rm -fr coverage
+
+set -e
 echo "Building..."
 $DC build web
 
@@ -12,26 +17,58 @@ cleanup() {
   echo "Cleaning up..."
   $DC stop
   $DC rm -f
+  rm -fr ./tmp
 }
 trap cleanup EXIT
 
-echo "Installing modules..."
-if ! $DC exec -T web npm install; then
-  exit 1
-fi
+mkdir tmp
+
+set +e
+EXIT_CODES=0
 
 echo "Linting..."
-if ! $DC exec -T web npm run eslint; then
-  exit 1
-fi
+$DC exec -T web npm run eslint > tmp/eslint.out
+EXIT_CODES=$(($EXIT_CODES + $?))
+
+echo "Gergiching..."
+gem which gergich
+cat tmp/eslint.out | sed 's/\/usr\/src\/app\///' | gergich capture eslint -
 
 echo "Running tests..."
-if ! $DC exec -T web npm run test:coverage; then
-  exit 1
+$DC exec -T web npm run test:coverage
+EXIT_CODES=$(($EXIT_CODES + $?))
+
+cat <<END > tmp/format_coverage.rb
+  def m(type, h)
+    total = h[type].to_i
+    covered = h["covered#{type}"].to_i
+    perc = (covered.to_f / total * 100).to_i
+    "#{perc}% (#{covered}/#{total})"
+  end
+
+  str = STDIN.readline
+  h = Hash[str.scan(/(\w+)="(\d+)"/)]
+  puts "Code coverage:"
+  puts "  Statements:   #{m('statements', h)}"
+  puts "  Conditionals: #{m('conditionals', h)}"
+  puts "  Methods:      #{m('methods', h)}"
+  puts "  Elements:     #{m('elements', h)}"
+END
+
+if [ "$EXIT_CODES" == "0" ]; then
+  echo "Copying coverage files from container $ID..."
+  docker cp $($DC ps -q web):/usr/src/app/coverage ./coverage
+  mv coverage/lcov-report/* coverage
+
+  COVERAGE=$(cat coverage/clover.xml | grep metrics | head -1 | ruby tmp/format_coverage.rb)
+  gergich message "$COVERAGE"
+
+  echo "Gergich publishing..."
+  gergich publish
+
+  echo "Done!"
+  exit 0
 fi
 
-echo "Copying coverage files from container $ID..."
-docker cp $($DC ps -q web):/usr/src/app/coverage ./coverage
-mv coverage/lcov-report/* coverage
-
-echo "Done!"
+echo "Failed with code $EXIT_CODES"
+exit $EXIT_CODES
